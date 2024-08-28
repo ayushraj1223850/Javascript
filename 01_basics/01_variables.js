c77d0dcc099fb42bd5f8585d9384a120d88a7678
ayushraj1223850 <120780090+ayushraj1223850@users.noreply.github.com>
2024-08-28 20:15:18 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accId = 1234554;
+let accEmail = "abc@gmail.com";
+var accPassword = "12345";
+accCity = "jalandhar";
+let accState;
+
+/* dont use var keyword  because of issue in block 
+ and functional scope */
+
+console.table([accId,accEmail,accPassword,accCity,accState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) // we are using node ja not browser
+
+// code readablity is very important
+
+/*DATATYPES*/
+
+/*number =>  2 to power 53
+bigint 
+string
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+
+//object
+
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined);//undefined
